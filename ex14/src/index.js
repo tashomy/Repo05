ebefd3ca58a6f439f4a95a002024d6917fb7d804
Natasa Mijatovic <mijatovicn650@gmail.com>
2021-09-17 21:14:48 +0200

    all tasks added

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb+".";
+    //Only change code above this line
+    return wordBlanks;
+}
+    
+console.log(main());
+    
+module.exports = main;
